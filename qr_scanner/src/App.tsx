82022e6510dd476446c0de2466e44bc7af6a73ec
mansi100 <119714573+mansi-100@users.noreply.github.com>
2025-06-13 12:52:32 +0530

    Add files via upload

---
+++ qr_scanner/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PhoneQrScanner from './PhoneQrScanner';
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneQrScanner />
+    </div>
+  );
+}
+
+export default App;
